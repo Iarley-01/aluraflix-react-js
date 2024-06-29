5e1decc314bf2ac638476e4994dff681b8ecf4a0
Iarley-01 <yarleyalves2016@gmail.com>
2024-06-29 17:41:17 -0300

    definindo cor de fundo padrão do projeto, criando página home e removendo arquivos desnecessários

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import "./App.css";
 
+import Home from "./pages/Home";
+
 function App() {
-  return <h1>Bem-Vindo ao Aluraflix</h1>;
+  return (
+    <>
+      <h1>Bem-Vindo ao Aluraflix</h1>
+      <Home />
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,9 @@
+import styles from "./Home.module.css";
+
+function Home() {
+  return (
+    <h2>HomePage</h2>
+  );
+}
+
+export default Home;
