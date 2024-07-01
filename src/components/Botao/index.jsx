78c2aae46eb5443174d233303395836762a4bd1f
Iarley-01 <yarleyalves2016@gmail.com>
2024-07-01 11:32:51 -0300

    implementando o botão de Link

--- src/components/Botao/index.jsx
+++ src/components/Botao/index.jsx
@@ -1,7 +1,15 @@
+import { NavLink } from "react-router-dom";
 import styles from "./Botao.module.css";
 
-function Botao() {
-  return ();
+
+function Botao({children}) {
+  return (
+    <NavLink
+      className={({ isActive }) => `${styles.link} ${isActive ? styles.linkAtivo : ""}`}
+    >
+      {children}
+    </NavLink>
+  );
 }
 
 export default Botao;
