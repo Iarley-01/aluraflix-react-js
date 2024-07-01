5da56e5839c3e74599db4a3d600d14534f409abf
Iarley-01 <yarleyalves2016@gmail.com>
2024-07-01 16:13:37 -0300

    implementando e estilizando o banner

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,7 +1,18 @@
 import styles from "./Banner.module.css";
 
 function Banner() {
-  return();
+  return (
+    <section className={styles.banner}>
+      <div className={styles.card}>
+        <div className={styles.textWrapper}>
+          <h1>FRONT-END</h1>
+          <h2>SEO com React</h2>
+          <p>Eu to aqui pra nesse vídeo dizer que a gente vai aprender a começar uma app inspirada no desenho Pokémon com Nextjs e React, ver algumas dicas sobre performance e de quebra conhecer uma plataforma sensacional pra fazer deploy que é a Vercel. Tudo em 22 minutos nesse vídeo feito com todo o carinho do mundo construindo uma "Pokedex"!</p>
+        </div>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/c8mVlakBESE?si=YbJIuSqYbTAo59aL" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      </div>
+    </section>
+  );
 }
 
 export default Banner;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,13 @@
 import styles from "./Home.module.css";
 
 import Header from "@/components/Header";
+import Banner from "@/components/Banner";
 
 function Home() {
   return (
     <>
       <Header />
+      <Banner />
     </>
   );
 }
