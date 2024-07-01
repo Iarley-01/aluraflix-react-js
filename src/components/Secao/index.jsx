56fce8f48268e7602def12d4ac0ee2ae27708879
Iarley-01 <yarleyalves2016@gmail.com>
2024-07-01 17:43:18 -0300

    implementando o componente das Seções

--- src/components/Secao/index.jsx
+++ src/components/Secao/index.jsx
@@ -1,7 +1,25 @@
 import styles from "./Secao.module.css";
 
 function Secao({ titulo }) {
-  return (<h3>{titulo.toUpperCase()}</h3>);
+  var corDeFundoDoTitulo;
+  if (titulo == "front-end") {
+    corDeFundoDoTitulo = { backgroundColor: "var(--front-end)"} 
+  } else if (titulo == "back-end") {
+    corDeFundoDoTitulo = { backgroundColor: "var(--back-end)"} 
+  } else {
+    corDeFundoDoTitulo = { backgroundColor: "var(--amarelo)"} 
+  }
+  
+  return (
+    <section className={styles.secao}>
+      <h3 style={corDeFundoDoTitulo} className={styles.secaoTitulo}>
+        {titulo.toUpperCase()}
+      </h3>
+      <div className={styles.videosContainer}>
+        
+      </div>
+    </section>
+  );
 }
 
 export default Secao;
