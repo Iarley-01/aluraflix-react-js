bdb476c9864120b018fa78c2a7c58040ce866b7e
Iarley-01 <yarleyalves2016@gmail.com>
2024-07-03 16:04:50 -0300

    criando estrutura da Lista Suspensa

---
+++ src/components/ListaSuspensa/index.jsx
@@ -0,0 +1,17 @@
+import styles from "./ListaSuspensa.module.css";
+
+function ListaSuspensa() {
+  return (
+    <div className={styles.campo}>
+      <label></label>
+      <select>
+        <option></option>
+        <option>FRONT-END</option>
+        <option>BACK-END</option>
+        <option>MOBILE</option>
+      </select>
+    </div>
+  );
+}
+
+export default ListaSuspensa;
